e8df9e8f6492509855381c0df52d020bfa7e7cfc
jtam11 <justinktam@gmail.com>
2016-05-31 19:49:22 -0700

    Added associations to talk model

--- app/models/talk.rb
+++ app/models/talk.rb
@@ -1,3 +1,6 @@
 class Talk < ActiveRecord::Base
   belongs_to :host, :class_name => "User"
+
+  has_many :invites, :foreign_key => "attended_talk_id"
+  has_many :attendees, :through => :invites
 end
