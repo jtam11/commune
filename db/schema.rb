102f49bec966ad2355bd5f1d52aad8786a890eaa
jtam11 <justinktam@gmail.com>
2016-05-31 18:01:02 -0700

    Created invite model

--- db/schema.rb
+++ db/schema.rb
@@ -11,10 +11,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160531225430) do
+ActiveRecord::Schema.define(version: 20160601005550) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "invites", force: :cascade do |t|
+    t.integer  "attendee_id"
+    t.integer  "attended_talk_id"
+    t.datetime "created_at",       null: false
+    t.datetime "updated_at",       null: false
+  end
 
   create_table "talks", force: :cascade do |t|
     t.string   "title"
---
+++ spec/factories/invites.rb
@@ -0,0 +1,6 @@
+FactoryGirl.define do
+  factory :invite do
+    attendee_id 1
+    attended_talk_id 1
+  end
+end
