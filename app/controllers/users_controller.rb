8462e0a3f9b9fb2d1cb3c214c88fdd6d826654e3
jtam11 <justinktam@gmail.com>
2016-06-01 11:19:29 -0700

    Added variables for upcoming/previous talks

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,7 +2,11 @@ class UsersController < ApplicationController
 
   def show
     @user = User.find(params[:id])
-    @talks = @user.talks.all.order("date asc", "start_time asc")
+    # grabbing all of the user's talk history (Not too DRY. Will refactor)
+    @upcoming_hosted = @user.upcoming_hosted.order("date asc", "start_time asc")
+    @upcoming_attended = @user.upcoming_attended.order("date asc", "start_time asc")
+    @previous_hosted = @user.previous_hosted.order("date asc", "start_time asc")
+    @previous_attended = @user.previous_attended.order("date asc", "start_time asc")
   end
 
 end
