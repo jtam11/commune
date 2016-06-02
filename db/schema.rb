7204afd5bcb24d2171e7f936a2ba59b8f4f027c6
jtam11 <justinktam@gmail.com>
2016-06-01 19:42:47 -0700

    Added occupation to user model

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160601005820) do
+ActiveRecord::Schema.define(version: 20160602024029) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -65,6 +65,7 @@ ActiveRecord::Schema.define(version: 20160601005820) do
     t.string   "username"
     t.string   "picture"
     t.string   "location"
+    t.string   "occupation"
   end
 
   add_index "users", ["confirmation_token"], name: "index_users_on_confirmation_token", unique: true, using: :btree
